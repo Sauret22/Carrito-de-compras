37b0c8f2cc6325e2dcf98145f9a65f75de32f858
Sauret <0246856@up.edu.mx>
2023-12-27 16:56:54 -0600

    Segundo modelo

--- Desktop/carro de compras/alfajores.py
+++ Desktop/carro de compras/alfajores.py
@@ -1,7 +1,31 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
+def on_entry_click(event, entry, placeholder):
+    if entry.get() == placeholder:
+        entry.delete(0, "end")
+        entry.config(fg='white')  # Cambiar color del texto cuando se empieza a escribir
+
+def validar_nombre(cadena):
+    return len(cadena) >= 7
+
+def validar_direccion(cadena):
+    return len(cadena) >= 10
+
+def validar_numero(cadena):
+    return 10 <= len(cadena) <= 10
 
 def iniciar():
+    nombre_valor = nombre.get()
+    direccion_valor = direccion.get()
+    numero_valor = numero.get()
+
+    if not (validar_nombre(nombre_valor) and validar_direccion(direccion_valor) and validar_numero(numero_valor)):
+        etiqueta_obligatorio.config(text="¡Campos inválidos!", fg="red")
+        return
+    else:
+        etiqueta_obligatorio.config(text="", fg="red")
+
     productos_seleccionados = []
 
     def agregar_al_carrito(producto, precio):
@@ -62,9 +86,17 @@ def iniciar():
     pagina_insta = tk.Label(nueva_ventana, text="@alfajores_elcoso", font=("cursive", 24, "italic"), bg='#800020', fg='white')
     pagina_insta.pack(pady=20)
 
-    boton_carrito = tk.Button(nueva_ventana, text="Carrito", command=lambda: mostrar_carrito(productos_seleccionados), font=("Helvetica", 14), bd=0, highlightthickness=0, fg='white')
-    boton_carrito.pack(side=tk.RIGHT, padx=20, pady=10)
-    boton_carrito.configure(bg=ventana.cget('bg'), activebackground=ventana.cget('bg'))
+    ruta_imagen = r'C:\Users\Sango\Desktop\carrito de compras\.vscode\carrito.png'
+    original_image = Image.open(ruta_imagen)
+    resized_image = original_image.resize((80, 80))  # Corrected attribute name
+    carrito_image = ImageTk.PhotoImage(resized_image)
+
+    # Carrito button with resized image
+    boton_carrito = tk.Button(nueva_ventana, image=carrito_image,
+                              command=lambda: mostrar_carrito(productos_seleccionados), bd=0, highlightthickness=0, bg='#800020')
+    boton_carrito.image = carrito_image
+    boton_carrito.place(relx=1, rely=0, x=-8, y=10, anchor="ne")
+
 
 
 def mostrar_carrito(productos_seleccionados):
@@ -91,11 +123,44 @@ ancho_pantalla = ventana.winfo_screenwidth()
 alto_pantalla = ventana.winfo_screenheight()
 ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}+0+0")
 
-titulo = tk.Label(ventana, text="EL COSO", font=("Cursive", 48, "italic"), bg='#800020', fg='white')
-titulo.place(relx=0.5, rely=0.5, anchor="center")
+# Título en la parte superior
+alfajores =tk.Label(ventana, text="Alfajores", font=("Cursive", 50, "italic"), bg='#800020', fg='white')
+alfajores.pack(side=tk.TOP, pady=40)
 
+coso = tk.Label(ventana, text="EL COSO", font=("Cursive", 70, "italic"), bg='#800020', fg='white')
+coso.pack(side=tk.TOP, pady=50)
+
+
+
+# Contenedor principal centrado
+contenedor_central = tk.Frame(ventana, bg='#800020')
+contenedor_central.pack(expand=True)
+
+# Etiqueta "obligatorio*"
+etiqueta_obligatorio = tk.Label(contenedor_central, text="", font=("Helvetica", 12), fg="red", bg='#800020')
+etiqueta_obligatorio.pack(pady=5)
+
+# Entry para el nombre con tamaño personalizado
+nombre = tk.Entry(contenedor_central, font=("Helvetica", 16), bd=1, relief="solid", fg='white', bg='#800020')
+nombre.insert(0, "Nombre y Apellido")
+nombre.bind('<FocusIn>', lambda event: on_entry_click(event, nombre, "Nombre y Apellido"))
+nombre.pack(pady=5, padx=20, fill=tk.X)
+
+# Entry para la dirección con tamaño personalizado
+direccion = tk.Entry(contenedor_central, font=("Helvetica", 16), bd=1, relief="solid", fg='white', bg='#800020')
+direccion.insert(0, "Dirección")  # Texto predeterminado
+direccion.bind('<FocusIn>', lambda event: on_entry_click(event, direccion, "Dirección"))
+direccion.pack(pady=5, padx=20, fill=tk.X)
+
+# Entry para el número con tamaño personalizado
+numero = tk.Entry(contenedor_central, font=("Helvetica", 16), bd=1, relief="solid", fg='white', bg='#800020')
+numero.insert(0, "Número")  # Texto predeterminado
+numero.bind('<FocusIn>', lambda event: on_entry_click(event, numero, "Número"))
+numero.pack(pady=5, padx=20, fill=tk.X)
+
+# Botón en la parte inferior
 boton_iniciar = tk.Button(ventana, text="Iniciar", command=iniciar, font=("Helvetica", 18), bd=0, highlightthickness=0, fg='white')
-boton_iniciar.place(relx=0.5, rely=0.9, anchor="center")
+boton_iniciar.pack(side=tk.BOTTOM, pady=70)
 boton_iniciar.configure(bg=ventana.cget('bg'))
 
 ventana.mainloop()
